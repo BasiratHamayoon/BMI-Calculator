1f7f30219bf2e91faab857113f4e4fa0115e29d9
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-12 11:51:12 +0500

    Git input feilds from HTML in js file

---
+++ script.js
@@ -0,0 +1,4 @@
+// Get input fields and button
+const weightInput = document.getElementById('weight');
+const heightInput = document.getElementById('height');
+const calculateBtn = document.getElementById('calculate-btn');
