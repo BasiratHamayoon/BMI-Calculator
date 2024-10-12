9fa543505a6f9684a03df6dc562137deb1c2eedd
Basirat Hamayoon <basirathumayun@gmail.com>
2024-10-12 11:53:13 +0500

    get the result elements from HTML file and add event listner function to perform action on button

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 const weightInput = document.getElementById('weight');
 const heightInput = document.getElementById('height');
 const calculateBtn = document.getElementById('calculate-btn');
+
+// Get result elements
+const bmiResult = document.getElementById('bmi-result');
+const bmiCategory = document.getElementById('bmi-category');
+
+// Add event listener to calculate button
+calculateBtn.addEventListener('click', calculateBMI);
